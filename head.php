f4770b260f64b9dfae00efd3760792433e206a27
wz2021111com <96029778+wz2021111com@users.noreply.github.com>
2022-12-02 21:44:05 +0800

    Add files via upload

---
+++ head.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(0);
+header("Content-type:text/html;charset=utf-8"); 
+echo '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>YiQQ</title>
+<META HTTP-EQUIV="Cache-Control" CONTENT="no-cache,no-store, must-revalidate"> 
+
+<META HTTP-EQUIV="pragma" CONTENT="no-cache"> 
+
+<META HTTP-EQUIV="expires" CONTENT="0">
+ <link rel="stylesheet" type="text/css" href="static/css/style1.css">
+</head>
+<body>
+';
+?>
